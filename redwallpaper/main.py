372e366ae182c51f8431740b383b70375e553855
dhuang-hub <56151894+dhuang-hub@users.noreply.github.com>
2025-07-16 00:28:08 -0400

    Improve package exports and logging

--- redwallpaper/main.py
+++ redwallpaper/main.py
@@ -1,15 +1,12 @@
-import redwallpaper.image.RedWallpaperImage
-import redwallpaper.image.utils
-import redwallpaper.buffer.FullsizeBuffer
-import redwallpaper.buffer.RedWallpaperScraper
-import redwallpaper.buffer.ScoredBuffer
-import redwallpaper.buffer.ThumbnailBuffer
-import redwallpaper.buffer.RedWallpaperBufferBase
-import redwallpaper.set_wallpaper
-import redwallpaper.RedWallpaperApp as RedWallpaperApp
+import argparse
+from redwallpaper.RedWallpaperApp import RedWallpaperApp
 
-def main():
-    RedWallpaperApp.RedWallpaperApp()
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(description="Run RedWallpaper")
+    parser.parse_args(argv)
+    RedWallpaperApp()
+
 
 if __name__ == '__main__':
     main()
--- redwallpaper/redwallpaper/__init__.py
+++ redwallpaper/redwallpaper/__init__.py
@@ -1,1 +1,15 @@
-from . import *
+from .RedWallpaperApp import RedWallpaperApp
+from .set_wallpaper import set_wallpaper
+from .image.RedWallpaperImage import RedWallpaperImage
+from .image import utils
+from .buffer.RedWallpaperScraper import RedWallpaperScraper
+from .buffer.ThumbnailBuffer import ThumbnailBuffer
+from .buffer.ScoredBuffer import ScoredBuffer
+from .buffer.FullsizeBuffer import FullsizeBuffer
+from .buffer.RedWallpaperBufferBase import RedWallpaperBufferBase
+
+__all__ = [
+    'RedWallpaperApp', 'set_wallpaper', 'RedWallpaperImage', 'utils',
+    'RedWallpaperScraper', 'ThumbnailBuffer', 'ScoredBuffer',
+    'FullsizeBuffer', 'RedWallpaperBufferBase'
+]
